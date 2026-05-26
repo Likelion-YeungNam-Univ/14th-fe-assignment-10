fad3de935a004a4f26b07dee404806859b8c6852
copepb <qwwo5015@gmail.com>
2026-05-26 12:44:35 +0900

    기본 틀

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-3xl text-red-600">App</div>;
+  return (
+    <div id="Main" className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <div id="box" className="p-8 min-w-[900px] min-h-[650px] bg-white rounded-2xl border-4 border-gray-200">
+        <div className="flex flex-col gap-5">
+          <div className="font-black text-6xl text-blue-600">대구 버스 도착 알리미</div>
+          <div className="font-medium text-xl text-blue-900">정류장 이름으로 버스 도착 정보를 확인하는 서비스입니다.</div>
+
+          <div className="mt-8 flex gap-8">
+            <input
+              placeholder="정류장 이름을 입력하세요."
+              className="flex-1 px-5 py-4 border-2 border-gray-200 rounded-xl outline-none text-lg focus:border-blue-500"
+            />
+
+            <button className="px-8 py-4 bg-blue-600 text-white font-bold rounded-xl hover:bg-blue-700">
+              검색
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
